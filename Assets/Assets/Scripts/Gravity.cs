60b524e85d968eb6ae88927192497d2457a5eab3
Artem Pavlov <pavl0ff@ya.ru>
2017-04-22 15:55:00 +0300

    added gravity

---
+++ Assets/Assets/Scripts/Gravity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Gravity : MonoBehaviour {
+ 
+    public GameObject[] objects;
+    public GameObject planet;
+ 
+    public float gravitationalPull;
+ 
+    void Start() {
+        objects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+    }
+
+    void FixedUpdate() {
+        //apply spherical gravity to selected objects (set the objects in editor)
+        foreach (GameObject o in objects) {
+			Rigidbody rb = o.GetComponent<Rigidbody>();
+            if (rb != null) {
+                rb.AddForce((planet.transform.position - o.transform.position).normalized * gravitationalPull);
+            }
+        }
+        //or apply gravity to all game objects with rigidbody
+        // foreach (GameObject o in UnityEngine.Object.FindObjectsOfType<GameObject>()) {
+        //     if(o.rigidbody && o != planet){
+        //         o.rigidbody.AddForce((planet.transform.position - o.transform.position).normalized * gravitationalPull);
+        //     }
+        // }
+    }
+ 
+}
