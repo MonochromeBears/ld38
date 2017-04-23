5c7a42a12df2cf8311cd26896b0b14518f728ffa
Sergei Melnikov <me@rnr.name>
2017-04-23 11:19:09 +0300

    Added score.

---
+++ Assets/Assets/Scripts/GUI/pGUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.FindObjectsOfType<HarvesterController> ().Length == 0) {
+			this.transform.Find ("GameOver").gameObject.SetActive (true);
+		}
+	}
+}
