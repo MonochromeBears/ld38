eb6c5d4a6f27ff86b2b02db4bf7c0ea66c6ba426
Maxim <raincoven@gmail.com>
2017-04-23 15:54:29 +0300

    Added: start game scene

---
+++ Assets/Assets/Scripts/StartGame/ButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadLevel() {
+		Debug.Log ("123123123");
+	}
+		
+}
